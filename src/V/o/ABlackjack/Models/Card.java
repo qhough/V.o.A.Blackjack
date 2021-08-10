2fd8911ce4adf1389dc616342c19743ce8e1e5f1
pullejam <79878433+pullejam@users.noreply.github.com>
2021-08-10 14:52:11 -0600

    Deck and Card

--- src/V/o/ABlackjack/Models/Card.java
+++ src/V/o/ABlackjack/Models/Card.java
@@ -1,47 +1,52 @@
 package V.o.ABlackjack.Models;
 
 public class Card {
-    private Faces value;
-    private Suit suit;
+        private Faces value;
+        private Suit suit;
 
-    public Card(Faces newValue, Suit newSuit){
-        value = newValue;
-        suit = newSuit;
-    }
+        public Card(Faces newValue, Suit newSuit){
+            this.value = newValue;
+            this.suit = newSuit;
+        }
 
-    public Object getValue(){
-        return value;
-    }
+        public int getValue(){
+            return value.getValue();
 
-    public Suit getSuit(){
-        return suit;
-    }
+        }
 
-    public String toString(){
-        String description = "";
-        if(value.getValue()!=10) {
-            description = String.valueOf(value.getValue());
-        }else{
-            if(value==Faces.TEN){
+        public Suit getSuit(){
+            return suit;
+        }
+
+
+
+
+
+        public String toString(){
+            String description = "";
+            if(value.getValue()!=10) {
                 description = String.valueOf(value.getValue());
-            }else if(value==Faces.JACK){
-                description = "Jack";
-            }else if(value==Faces.QUEEN){
-                description = "Queen";
             }else{
-                description = "King";
+                if(value==Faces.TEN){
+                    description = String.valueOf(value.getValue());
+                }else if(value==Faces.JACK){
+                    description = "Jack";
+                }else if(value==Faces.QUEEN){
+                    description = "Queen";
+                }else{
+                    description = "King";
+                }
             }
+            description += " of ";
+            if(suit==Suit.HEARTS){
+                description += "Hearts";
+            }else if (suit==Suit.SPADES){
+                description += "Spades";
+            }else if (suit==Suit.CLUBS){
+                description += "Clubs";
+            }else{
+                description += "Diamonds";
+            }
+            return description;
         }
-        description += " of ";
-        if(suit==Suit.HEARTS){
-            description += "Hearts";
-        }else if (suit==Suit.SPADES){
-            description += "Spades";
-        }else if (suit==Suit.CLUBS){
-            description += "Clubs";
-        }else{
-            description += "Diamonds";
-        }
-        return description;
     }
-}
--- src/V/o/ABlackjack/Models/Deck.java
+++ src/V/o/ABlackjack/Models/Deck.java
@@ -1,128 +1,80 @@
 package V.o.ABlackjack.Models;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Deck {
-    public void generateDeck(){
-        Card[][] newDeck = new Card[4][13];
-        for(int currSuit=0; currSuit<=3; currSuit++){
-            for(int currValue=0; currValue<=12; currValue++){
-                if(currSuit==0){
-                    if(currValue==0) {
-                        newDeck[0][currValue]= new Card(Faces.ACE,Suit.HEARTS);
-                    }else if(currValue==1){
-                        newDeck[0][currValue]= new Card(Faces.TWO,Suit.HEARTS);
-                    }else if(currValue==2){
-                        newDeck[0][currValue]= new Card(Faces.THREE,Suit.HEARTS);
-                    }else if(currValue==3){
-                        newDeck[0][currValue]= new Card(Faces.FOUR,Suit.HEARTS);
-                    }else if(currValue==4){
-                        newDeck[0][currValue]= new Card(Faces.FIVE,Suit.HEARTS);
-                    }else if(currValue==5){
-                        newDeck[0][currValue]= new Card(Faces.SIX,Suit.HEARTS);
-                    }else if(currValue==6){
-                        newDeck[0][currValue]= new Card(Faces.SEVEN,Suit.HEARTS);
-                    }else if(currValue==7){
-                        newDeck[0][currValue]= new Card(Faces.EIGHT,Suit.HEARTS);
-                    }else if(currValue==8){
-                        newDeck[0][currValue]= new Card(Faces.NINE,Suit.HEARTS);
-                    }else if(currValue==9){
-                        newDeck[0][currValue]= new Card(Faces.TEN,Suit.HEARTS);
-                    }else if(currValue==10){
-                        newDeck[0][currValue]= new Card(Faces.JACK,Suit.HEARTS);
-                    }else if(currValue==11){
-                        newDeck[0][currValue]= new Card(Faces.QUEEN,Suit.HEARTS);
-                    }else{
-                        newDeck[0][currValue]= new Card(Faces.KING,Suit.HEARTS);
-                    }
-                }else if(currSuit==1){
-                    if(currValue==0) {
-                        newDeck[1][currValue]= new Card(Faces.ACE,Suit.SPADES);
-                    }else if(currValue==1){
-                        newDeck[1][currValue]= new Card(Faces.TWO,Suit.SPADES);
-                    }else if(currValue==2){
-                        newDeck[1][currValue]= new Card(Faces.THREE,Suit.SPADES);
-                    }else if(currValue==3){
-                        newDeck[1][currValue]= new Card(Faces.FOUR,Suit.SPADES);
-                    }else if(currValue==4){
-                        newDeck[1][currValue]= new Card(Faces.FIVE,Suit.SPADES);
-                    }else if(currValue==5){
-                        newDeck[1][currValue]= new Card(Faces.SIX,Suit.SPADES);
-                    }else if(currValue==6){
-                        newDeck[1][currValue]= new Card(Faces.SEVEN,Suit.SPADES);
-                    }else if(currValue==7){
-                        newDeck[1][currValue]= new Card(Faces.EIGHT,Suit.SPADES);
-                    }else if(currValue==8){
-                        newDeck[1][currValue]= new Card(Faces.NINE,Suit.SPADES);
-                    }else if(currValue==9){
-                        newDeck[1][currValue]= new Card(Faces.TEN,Suit.SPADES);
-                    }else if(currValue==10){
-                        newDeck[1][currValue]= new Card(Faces.JACK,Suit.SPADES);
-                    }else if(currValue==11){
-                        newDeck[1][currValue]= new Card(Faces.QUEEN,Suit.SPADES);
-                    }else{
-                        newDeck[1][currValue]= new Card(Faces.KING,Suit.SPADES);
-                    }
+    private static final ArrayList<Card> deck = new ArrayList<>();
 
-                }else if(currSuit==2){
-                    if(currValue==0) {
-                        newDeck[2][currValue]= new Card(Faces.ACE,Suit.CLUBS);
-                    }else if(currValue==1){
-                        newDeck[2][currValue]= new Card(Faces.TWO,Suit.CLUBS);
-                    }else if(currValue==2){
-                        newDeck[2][currValue]= new Card(Faces.THREE,Suit.CLUBS);
-                    }else if(currValue==3){
-                        newDeck[2][currValue]= new Card(Faces.FOUR,Suit.CLUBS);
-                    }else if(currValue==4){
-                        newDeck[2][currValue]= new Card(Faces.FIVE,Suit.CLUBS);
-                    }else if(currValue==5){
-                        newDeck[2][currValue]= new Card(Faces.SIX,Suit.CLUBS);
-                    }else if(currValue==6){
-                        newDeck[2][currValue]= new Card(Faces.SEVEN,Suit.CLUBS);
-                    }else if(currValue==7){
-                        newDeck[2][currValue]= new Card(Faces.EIGHT,Suit.CLUBS);
-                    }else if(currValue==8){
-                        newDeck[2][currValue]= new Card(Faces.NINE,Suit.CLUBS);
-                    }else if(currValue==9){
-                        newDeck[2][currValue]= new Card(Faces.TEN,Suit.CLUBS);
-                    }else if(currValue==10){
-                        newDeck[2][currValue]= new Card(Faces.JACK,Suit.CLUBS);
-                    }else if(currValue==11){
-                        newDeck[2][currValue]= new Card(Faces.QUEEN,Suit.CLUBS);
-                    }else{
-                        newDeck[2][currValue]= new Card(Faces.KING,Suit.CLUBS);
-                    }
-
-                }else{
-                    if(currValue==0) {
-                        newDeck[3][currValue]= new Card(Faces.ACE,Suit.DIAMONDS);
-                    }else if(currValue==1){
-                        newDeck[3][currValue]= new Card(Faces.TWO,Suit.DIAMONDS);
-                    }else if(currValue==2){
-                        newDeck[3][currValue]= new Card(Faces.THREE,Suit.DIAMONDS);
-                    }else if(currValue==3){
-                        newDeck[3][currValue]= new Card(Faces.FOUR,Suit.DIAMONDS);
-                    }else if(currValue==4){
-                        newDeck[3][currValue]= new Card(Faces.FIVE,Suit.DIAMONDS);
-                    }else if(currValue==5){
-                        newDeck[3][currValue]= new Card(Faces.SIX,Suit.DIAMONDS);
-                    }else if(currValue==6){
-                        newDeck[3][currValue]= new Card(Faces.SEVEN,Suit.DIAMONDS);
-                    }else if(currValue==7){
-                        newDeck[3][currValue]= new Card(Faces.EIGHT,Suit.DIAMONDS);
-                    }else if(currValue==8){
-                        newDeck[3][currValue]= new Card(Faces.NINE,Suit.DIAMONDS);
-                    }else if(currValue==9){
-                        newDeck[3][currValue]= new Card(Faces.TEN,Suit.DIAMONDS);
-                    }else if(currValue==10){
-                        newDeck[3][currValue]= new Card(Faces.JACK,Suit.DIAMONDS);
-                    }else if(currValue==11){
-                        newDeck[3][currValue]= new Card(Faces.QUEEN,Suit.DIAMONDS);
-                    }else{
-                        newDeck[3][currValue]= new Card(Faces.KING,Suit.DIAMONDS);
-                    }
-                }
-            }
-        }
-        //deck = newDeck;
+    public static ArrayList<Card>  deckGenerator(){
+        ArrayList<Card> deck = new ArrayList<>();
+        deck.add(new Card(Faces.TWO, Suit.HEARTS));
+        deck.add(new Card(Faces.TWO, Suit.DIAMOND));
+        deck.add(new Card(Faces.TWO, Suit.CLUBS));
+        deck.add(new Card(Faces.TWO, Suit.SPADES));
+        deck.add(new Card(Faces.THREE, Suit.DIAMOND));
+        deck.add(new Card(Faces.THREE, Suit.HEARTS));
+        deck.add(new Card(Faces.THREE, Suit.CLUBS));
+        deck.add(new Card(Faces.THREE, Suit.SPADES));
+        deck.add(new Card(Faces.FOUR, Suit.DIAMOND));
+        deck.add(new Card(Faces.FOUR, Suit.HEARTS));
+        deck.add(new Card(Faces.FOUR, Suit.CLUBS));
+        deck.add(new Card(Faces.FOUR, Suit.SPADES));
+        deck.add(new Card(Faces.FIVE, Suit.DIAMOND));
+        deck.add(new Card(Faces.FIVE, Suit.HEARTS));
+        deck.add(new Card(Faces.FIVE, Suit.CLUBS));
+        deck.add(new Card(Faces.FIVE, Suit.SPADES));
+        deck.add(new Card(Faces.SIX, Suit.DIAMOND));
+        deck.add(new Card(Faces.SIX, Suit.HEARTS));
+        deck.add(new Card(Faces.SIX, Suit.CLUBS));
+        deck.add(new Card(Faces.SIX, Suit.SPADES));
+        deck.add(new Card(Faces.SEVEN, Suit.DIAMOND));
+        deck.add(new Card(Faces.SEVEN, Suit.HEARTS));
+        deck.add(new Card(Faces.SEVEN, Suit.CLUBS));
+        deck.add(new Card(Faces.SEVEN, Suit.SPADES));
+        deck.add(new Card(Faces.EIGHT, Suit.DIAMOND));
+        deck.add(new Card(Faces.EIGHT, Suit.HEARTS));
+        deck.add(new Card(Faces.EIGHT, Suit.CLUBS));
+        deck.add(new Card(Faces.EIGHT, Suit.SPADES));
+        deck.add(new Card(Faces.NINE, Suit.DIAMOND));
+        deck.add(new Card(Faces.NINE, Suit.HEARTS));
+        deck.add(new Card(Faces.NINE, Suit.CLUBS));
+        deck.add(new Card(Faces.NINE, Suit.SPADES));
+        deck.add(new Card(Faces.TEN, Suit.DIAMOND));
+        deck.add(new Card(Faces.TEN, Suit.HEARTS));
+        deck.add(new Card(Faces.TEN, Suit.CLUBS));
+        deck.add(new Card(Faces.TEN, Suit.SPADES));
+        deck.add(new Card(Faces.JACK, Suit.DIAMOND));
+        deck.add(new Card(Faces.JACK, Suit.HEARTS));
+        deck.add(new Card(Faces.JACK, Suit.CLUBS));
+        deck.add(new Card(Faces.JACK, Suit.SPADES));
+        deck.add(new Card(Faces.QUEEN, Suit.DIAMOND));
+        deck.add(new Card(Faces.QUEEN, Suit.HEARTS));
+        deck.add(new Card(Faces.QUEEN, Suit.CLUBS));
+        deck.add(new Card(Faces.QUEEN, Suit.SPADES));
+        deck.add(new Card(Faces.KING, Suit.DIAMOND));
+        deck.add(new Card(Faces.KING, Suit.HEARTS));
+        deck.add(new Card(Faces.KING, Suit.CLUBS));
+        deck.add(new Card(Faces.KING, Suit.SPADES));
+        deck.add(new Card(Faces.ACE, Suit.DIAMOND));
+        deck.add(new Card(Faces.ACE, Suit.HEARTS));
+        deck.add(new Card(Faces.ACE, Suit.CLUBS));
+        deck.add(new Card(Faces.ACE, Suit.SPADES));
+        return deck;
     }
+    public static void shuffleDeck(){
+        Collections.shuffle(deck);
+    }
+
+    public void moveCardToBottom(){
+        Card ph = deck.get(0);
+        deck.remove(0);
+        deck.add(ph);
+    }
+
+    public ArrayList<Card> getDeck(){
+        return deck;
+    }
+
 
 }
